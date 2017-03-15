0ddd6de08487000e9d656007ac5ae80044dabd48
MoonAntonio <pinchodev91@gmail.com>
2017-03-15 13:39:06 +0100

    Add escena test

---
+++ Gestor Escenas Project/Assets/Gestor Escenas/Scripts/Sistema/SProteo.cs
@@ -0,0 +1,23 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// SProteo.cs (15/03/2017)														\\
+// Autor: Antonio Mateo (Moon Pincho) 									        \\
+// Descripcion:		Sistema que gestiona el cambio de escenas.					\\
+// Fecha Mod:		15/03/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonPincho
+{
+	/// <summary>
+	/// <para>Sistema que gestiona el cambio de escenas</para>
+	/// </summary>
+	public class SProteo : MonoBehaviour 
+	{
+
+	}
+}
